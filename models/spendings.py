f662f828e81325fef1c4f3f7f246283162e71696
Nthx <ntfm95@gmail.com>
2020-09-30 22:38:35 -0300

    feature/talkers

--- models/spendings.py
+++ models/spendings.py
@@ -1,4 +1,7 @@
 from app import db
 
 class Spedings(db.Model):
-    id = db.Column(db.Integer, primary_key = True)    
+    id = db.Column(db.Integer, primary_key = True)
+
+    debts = db.relantionship('Debts', backref = 'debts', lazy = True)
+    credit_card = db.relantionship('CreditCardDebts', backref = 'credit_card_debts', lazy = True)
