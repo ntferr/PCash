737c3195481104131623f3f62fad5b1bd1103d56
Nthx <ntfm95@gmail.com>
2020-10-02 21:57:49 -0300

    fixes and updates

--- models/debts.py
+++ models/debts.py
@@ -1,11 +1,11 @@
 from app import db
 
 class Debts(db.Model):
-    id = db.column(db.Integer, primary_key = True, autoincrement = True)
-    value = db.column(db.Float)
-    description = db.column(db.String(180))
-    due_date = db.column(db.DateTime)
-    document_date = db.column(db.DateTime)
+    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    value = db.Column(db.Float)
+    description = db.Column(db.String(180))
+    due_date = db.Column(db.DateTime)
+    document_date = db.Column(db.DateTime)
 
     spending_id = db.Column(db.Integer, db.ForeignKey('spending.debts'), nullable = False)    
 
@@ -14,3 +14,5 @@ class Debts(db.Model):
         self.description = debts['description']
         self.due_date = debts['due_date']
         self.document_date = debts['document_date']
+
+        self.spending_id = debts['spending_id']
--- models/spendings.py
+++ models/spendings.py
@@ -1,7 +1,13 @@
 from app import db
 
-class Spedings(db.Model):
+class Spendings(db.Model):
     id = db.Column(db.Integer, primary_key = True)
 
-    debts = db.relantionship('Debts', backref = 'debts', lazy = True)
-    credit_card = db.relantionship('CreditCardDebts', backref = 'credit_card_debts', lazy = True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.spendings'), nullable=False)
+
+    debts = db.relationship('Debts', backref = 'debts', lazy = True)
+    credit_card = db.relationship('CreditCardDebts', backref = 'credit_card_debts', lazy = True)
+
+
+def __init__(self, spendings)
+    self.user_id = spendings['user_id']
